7647d05be0391e4f73d9536f28c8c60b37d5e6a6
Shubh Agrawal <shubh@sas.upenn.edu>
2024-10-12 20:15:43 -0400

    start simim

--- camb/obj.py
+++ camb/obj.py
@@ -11,9 +11,9 @@ from astropy.cosmology import Planck18 as cosmo
 class AttrDict(dict):
     def __getattr__(self, key):
         return self[key]
-
+    
     def __setattr__(self, key, value):
         self[key] = value
-        
+    
     def __delattr__(self, key):
         del self[key]
---
+++ camb/window.py
@@ -0,0 +1,7 @@
+# BSD 3-Clause License
+# 
+# Copyright (c) 2024 Shubh Agrawal
+# All rights reserved.
+
+# windowing or k-space PSF functions
+
